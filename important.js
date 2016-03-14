bde955e5c08282269b84f2578cdf7200bc3c61a6
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-03-14 12:45:06 +0300

    [#115571305] Integrating Codecov

--- important.js
+++ important.js
@@ -11,3 +11,5 @@ mocha -r jscoverage --covout=html --covinject=true --coverage=90,85,75 test
 // lcov file for back-end
 
 istanbul cover ./node_modules/mocha/bin/_mocha -- -R spec
+
+export CODECOV_TOKEN=f086d79d-37bf-4ebe-8ffb-c6f31bc67e63
